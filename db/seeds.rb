5bd82e90edc298dc330f912b3f10ac4abe37144d
PrithiveeK <prithivee@codingmart.com>
2020-11-06 13:17:07 +0530

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+porducts = Product.create([{name: 'sphere'}, {name: 'smiley', price: "1", status: 'PURCHASE'}])
