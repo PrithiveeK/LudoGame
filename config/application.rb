8644fa5f8e9f202a2ff6911e22c4ec21a80a274e
PrithiveeK <prithivee@codingmart.com>
2020-11-06 14:57:30 +0530

    changed dotenv

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require 'carrierwave/orm/activerecord'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
+# Dotenv::Railtie.load
 
 
 module Ludo
